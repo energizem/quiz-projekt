37a6fe95a071a48092c5d64ea555fe08886835b6
Borislav Miletic <borislav.miletich@gmail.com>
2025-06-28 18:42:23 +0200

    nova baza3

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 // ******************************************************
 // password zastita min
